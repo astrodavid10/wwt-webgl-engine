3e864c00d3ab4b089b49b6a3e29c201f0d69861c
Jon Carifio <carifio24@gmail.com>
2022-07-09 02:04:15 -0400

    Make orientation of FolderView (horizontal/vertical) be deterined by a prop.

--- embed/src/FolderView.vue
+++ embed/src/FolderView.vue
@@ -2,15 +2,16 @@
   <div
     class="fv-root"
     v-if="items !== null"
+    :style="cssVars"
   >
     <div
-      class="item"
+      :class="['item', lastSelectedItem === item ? 'selected' : '']"
       v-for="item of items"
       :key="item.get_name()"
       :title="item.get_name()"
       @click="() => selectItem(item)"
     >
-      <img :src="item.get_thumbnailUrl()" />
+      <img :src="item.get_thumbnailUrl()" :alt="item.get_name()" />
       <div
         class="item-name"
       >{{item.get_name()}}</div>
@@ -28,6 +29,7 @@ import { ImageSetType, Thumbnail } from "@wwtelescope/engine-types";
 @Component
 export default class FolderView extends Vue {
   @Prop() rootFolder!: Folder;
+  @Prop() flexDirection!: "row" | "column";
   items: Thumbnail[] | null = null;
   lastSelectedItem: Thumbnail | null = null;
 
@@ -77,15 +79,22 @@ export default class FolderView extends Vue {
     }
   }
 
+  get cssVars() {
+    return {
+      "--flex-direction": this.flexDirection
+    };
+  }
+
 }
 </script>
 
 <style scoped lang="less">
 .fv-root {
   display: flex;
-  flex-direction: row;
+  flex-direction: var(--flex-direction);
   width: auto;
   overflow-x: auto;
+  background: black;
 
   &::-webkit-scrollbar {
     padding: 1px;
@@ -109,13 +118,18 @@ export default class FolderView extends Vue {
   padding: 3px;
   border: 1px solid white;
   border-radius: 2px;
-  width: 96px;
+  width: ~"min(96px, 16vw)";
   cursor: pointer;
 
   & img {
-    width: 96px;
-    height: 45px;
+    width: 100%;
+    height: ~"min(45px, 7.5vw)";
+    object-fit: cover;
     border-radius: 2px;
+  }
+
+  &.selected {
+    border: 1px solid dodgerblue;
   }
 }
 
